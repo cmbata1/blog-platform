30b6780fd62fc65b45004a3c77e54db436b03eec
cmbata1 <catherine.mbata@duke.edu>
2024-02-25 22:12:26 -0600

    wrap in div

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,21 +1,22 @@
 <template>
-    <section class="hero" style="background-color: #538152;">
-        <div class="hero-body" style="align-content: center; margin: auto;">
-            <div class="container" style="text-align: center;">
-                <p class="title is-1" style="font-family: 'Brush Script MT', cursive;" v-text="blog.Title"></p>
-                <p class="subtitle is-3" v-text="blog.date"/>
+    <div>
+        <section class="hero" style="background-color: #538152;">
+            <div class="hero-body" style="align-content: center; margin: auto;">
+                <div class="container" style="text-align: center;">
+                    <p class="title is-1" style="font-family: 'Brush Script MT', cursive;" v-text="blog.Title"/>
+                    <p class="subtitle is-3" v-text="blog.date"/>
+                </div>
             </div>
+        </section>
+        <b-loading :is-full-page="false" :active="isLoading" :can-cancel="false"></b-loading>
+        <div class="columns is-multiline" v-if="!isLoading && blog">
+            <div class="column is-one-quarter"></div>
+            <div class="column">
+                <p class="subtitle is-5" style="color: #000;" v-text="blog.Content"/>
+            </div>
+            <div class="column is-one-quarter"></div>
         </div>
-    </section>
-    <b-loading :is-full-page="false" :active="isLoading" :can-cancel="false"></b-loading>
-    <div class="columns is-multiline" v-if="!isLoading && blog">
-        <div class="column is-one-quarter"></div>
-        <div class="column">
-            <p class="subtitle is-5" style="color: #000;" v-text="blog.Content"/>
-        </div>
-        <div class="column is-one-quarter"></div>
     </div>
-
 </template>
   
   <script>
