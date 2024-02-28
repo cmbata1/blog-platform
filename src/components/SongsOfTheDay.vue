6e84ee471fb65756d750e43a6e1b118db008faaa
cmbata1 <catherine.mbata@duke.edu>
2024-02-27 23:12:01 -0600

    remove unworking video

--- src/components/SongsOfTheDay.vue
+++ src/components/SongsOfTheDay.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <section class="hero" is-light>
+        <section class="hero is-light">
             <div class="hero-body" style="align-content: center; margin: auto;">
                 <p class="title">Songs of the Day</p>
-                <iframe width="1045" height="622" src="https://www.youtube.com/embed/_iTcSs6sRok" title="Chill Kill" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </section>
     </div>
