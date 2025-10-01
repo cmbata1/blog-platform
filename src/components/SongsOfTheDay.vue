5cf326e4385557e4f802b423ea8529ee642cf96f
Catherine Mbata <cmm199@duke.edu>
2025-09-30 21:01:25 -0400

    fix songs of the day

--- src/components/SongsOfTheDay.vue
+++ src/components/SongsOfTheDay.vue
@@ -1,20 +1,37 @@
 <template>
-    <div>
-        <section class="hero is-light">
-            <div class="hero-body" style="padding: 5% 0% 0% 20%;">
-              <p class="title">Songs of the Day</p>
-            </div>
-        </section>
-    </div>
-</template>
-  
-  <script>
-  import Vue from 'vue'
-  import Buefy from 'buefy';
-  import 'buefy/dist/buefy.css';
+  <div>
+    <section class="hero is-light">
+      <div class="hero-body" style="padding: 5% 0% 0% 20%;">
+        <p class="title">Songs of the Day</p>
+      </div>
+    </section>
 
-  Vue.use(Buefy)
-  export default {
-    name: "SongsOfTheDay",
-  };
-  </script>
+    <section class="section" style="padding-top: 2rem;">
+      <div class="container" style="max-width: 860px;">
+        <div class="box" style="overflow:hidden; border-radius:0.5rem;">
+          <iframe
+            width="100%"
+            height="360"
+            src="https://www.youtube.com/embed/xlyrt5eAtKI"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+export default {
+  name: "SongsOfTheDay",
+}
+</script>
